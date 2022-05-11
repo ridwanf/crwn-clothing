464e529e9c45ac282387cb3f52601a84bfc517cb
ridwan fansuri <ridwan.fansuri@dana.id>
2022-05-11 08:24:09 +0700

    remove logger at production

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import {compose, createStore, applyMiddleware} from 'redux';
 import logger  from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-const middleWare = [logger]
+const middleWare = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
 // call this before action called
 const composedEnhancers = compose(applyMiddleware(...middleWare));
 export const store = createStore(rootReducer, undefined, composedEnhancers)
