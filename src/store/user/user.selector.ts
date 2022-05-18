8e4c12c9ac2e917934ff0e7efb7944bc71019cda
ridwan fansuri <ridwan.fansuri@dana.id>
2022-05-18 15:18:12 +0700

    update user selector

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,4 +1,3 @@
-import { UserData } from "../../utils/firebase/firebase.utils";
 import { UserState } from "./user.reducer";
 
-export const selectCurrentUser = (state: UserData): UserState => state.user.selectCurrentUser
+export const selectCurrentUser = (state): UserState => state.user.selectCurrentUser
